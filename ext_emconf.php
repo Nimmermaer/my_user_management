251b3eac026f21e2c55b34a6c8455e30d37c2ce6
Michael Blunck <michael.blunck@webicorns.com>
2023-06-13 11:16:22 +0200

    [TASK] Update composer

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,8 +13,8 @@ $EM_CONF['my_user_management'] = [
     'author_company' => 'Rotterdam School of Management, Erasmus University',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.4.0-11.4.99',
-            'beuser' => '11.4.0-11.4.99',
+            'typo3' => '12.4.99',
+            'beuser' => '12.4.99',
         ],
         'conflicts' => [],
         'suggests' => [],
